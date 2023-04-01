3b755a36b6eb3223610786d34a304ae01cd1b961
tanjinaesha <eshaislam15@gmail.com>
2023-04-01 12:24:43 +0600

    qns four addded

--- Solution/src/components/Blog/Blog.js
+++ Solution/src/components/Blog/Blog.js
@@ -13,6 +13,7 @@ When a Component mounts, a data structure called the state is created with a pre
 hi there
 <h1>Purpose of useEffect other than fetching data.</h1>
 <p>useEffect hook will send a network call. When that fetch resolves, the setState function will be used to set the server response to the local state. The component will then display as a result, updating the DOM with the new data.  </p>
+<h1>How does React works?</h1>
 
         </>
     );
