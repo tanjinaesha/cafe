6e802e88be7274edba078c554817ad0a2465ba09
tanjinaesha <eshaislam15@gmail.com>
2023-04-01 06:26:10 +0600

    newtwo

--- Solution/src/App.js
+++ Solution/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import CardBlog from './components/CardBlog/CardBlog';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
+import Blog from './components/Blog/Blog';
 
 function App() {
   return (
     <div className="App">
     <Header></Header>
       <CardBlog></CardBlog>
+      <Blog></Blog>
     </div>
   );
 }
